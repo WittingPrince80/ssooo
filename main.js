48ac4690e37955b122499444515be8c99310de03
WittingPrince80 <87693035+WittingPrince80@users.noreply.github.com>
2022-10-04 19:03:22 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,82 @@
+
+objects = [];
+status = "";
+
+function preload(){
+  video = createVideo('video1.mp4');
+  song = loadSound('alarm.mp3')
+}
+
+
+function setup() {
+  canvas = createCanvas(480, 380);
+  canvas.center();
+  video.hide();
+}
+
+function start()
+{
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+  console.log("Model Loaded!")
+  status = true;
+  video.loop();
+  video.speed(1);
+  video.volume(1);
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+
+
+
+function draw() {
+  image(video, 0, 0, 480, 380);
+      if(status != "")
+      {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+         
+          if(objects[i] &= person)
+          {
+            song.play();
+            song.setVolume(1);
+            song.rate(1);
+            document.getElementById("status").innerHTML = "Status : Baby Unfound";
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+          }
+          if(objects[i] == person)
+          document.getElementById("status").innerHTML = "Status : Baby Detected";
+          document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+ objects.length;
+          song.setVolume(0);
+          fill("#FF0000");
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+          noFill();
+          stroke("#FF0000");
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+        }
+      
+      }
+      if(objects.length < 0)
+      {
+        song.play();
+        song.setVolume(1);
+        song.rate(1);
+        document.getElementById("status").innerHTML = "Status : Baby Unfound";
+      }
+}
